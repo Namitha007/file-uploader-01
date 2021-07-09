cc4f3abaa2254a2a78b8479b108f809d96b70691
Namitha007 <9591581780om@gmail.com>
2021-07-10 00:06:02 +0530

    updated

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+  
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   images : string[] = [];
+   myForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    file: new FormControl('', [Validators.required]),
+    fileSource: new FormControl('', [Validators.required])
+  });
+   
+  constructor(private http: HttpClient) { }
+   
+  get f(){
+    return this.myForm.controls;
+  }
+   
+  onFileChange(event:any) {
+    if (event.target.files && event.target.files[0]) {
+        var filesAmount = event.target.files.length;
+        for (let i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
+   
+                reader.onload = (event:any) => {
+                  console.log(event.target.result);
+                   this.images.push(event.target.result); 
+   
+                   this.myForm.patchValue({
+                      fileSource: this.images
+                   });
+                }
+  
+                reader.readAsDataURL(event.target.files[i]);
+        }
+    }
+  } 
+    
+  submit(){
+    console.log(this.myForm.value);
+    this.http.post('http://localhost:80/upload.php', this.myForm.value) //here backend link should be connected
+      .subscribe(res => {
+        console.log(res);
+        alert('Uploaded Successfully.');
+      })
+  }
+}
